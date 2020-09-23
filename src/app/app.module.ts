f2a0248ff7ddec019d5c3a1e34a5589c7335d302
madistwn <madis.apsalon@twn.ee>
2020-09-23 16:20:52 +0300

    EHH-17 - Add communication with EtLex API - Add prefix interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EtLexApiService } from './api/et-lex-api.service';
+import { TopicsService } from './services/topics/topics.service';
+import { HomeComponent } from './features/home/home.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { PrefixInterceptor } from './services/interceptors/prefix.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    EtLexApiService,
+    TopicsService,
+    { provide: HTTP_INTERCEPTORS, useClass: PrefixInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
