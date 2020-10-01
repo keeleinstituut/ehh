e08068bccfee941fdd821f038b71534c04afd629
madistwn <madis.apsalon@twn.ee>
2020-10-01 09:35:05 +0300

    EHH-20 Modify header logos displaying

--- src/app/components/topics-list/topics-list.component.ts
+++ src/app/components/topics-list/topics-list.component.ts
@@ -10,11 +10,12 @@ import { Router } from '@angular/router';
 export class TopicsListComponent implements OnInit {
   @Input() topics: TopicItem[] = [];
 
-  countColors: string[] = ['#EADFE7', '#E5E1F5', '#E3E6FF', '#E1F4F7', '#F6E7CE', '#FDF1CC', '#E1EAD7', '#F3F6E4', '#FEE0E0', '#FFE9F9'];
+  topicColors: string[] = ['#EADFE7', '#E5E1F5', '#E3E6FF', '#E1F4F7', '#F6E7CE', '#FDF1CC', '#E1EAD7', '#F3F6E4', '#FEE0E0', '#FFE9F9'];
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+
   }
 
   async goToTopic(id: number): Promise<void> {
--- src/app/services/states/states.service.ts
+++ src/app/services/states/states.service.ts
@@ -5,11 +5,13 @@ import { TopicInfoDto, TopicInfoItem, TopicsDto } from '../api/api.models';
 export interface States {
   topicsList: TopicsDto;
   currentTopic: TopicInfoItem;
+  topicColors: string[];
 }
 
 const states = {
   topicsList: [],
   currentTopic: {},
+  topicColors: ['#EADFE7', '#E5E1F5', '#E3E6FF', '#E1F4F7', '#F6E7CE', '#FDF1CC', '#E1EAD7', '#F3F6E4', '#FEE0E0', '#FFE9F9'],
 };
 
 @Injectable()
