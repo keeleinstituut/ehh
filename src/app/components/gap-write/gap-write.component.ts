37f120bcc51ac444ef7d5630cff5019e18454067
madistwn <madis.apsalon@twn.ee>
2020-10-22 09:34:58 +0300

    EHH-3 Add gap-write component

--- src/app/components/gap-write/gap-write.component.ts
+++ src/app/components/gap-write/gap-write.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ehh-gap-write',
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gap-write.component.scss']
 })
 export class GapWriteComponent implements OnInit {
+  @Input() soundPath: string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  private async getSoundFile(audioContext, filepath): Promise<AudioBuffer> {
+    const response = await fetch(filepath);
+    const arrayBuffer = await response.arrayBuffer();
+    return await audioContext.decodeAudioData(arrayBuffer);
+  }
+
+  private playSound(audioContext, audioBuffer): void {
+    const sampleSource = audioContext.createBufferSource();
+    sampleSource.buffer = audioBuffer;
+    sampleSource.connect(audioContext.destination);
+    sampleSource.start();
+  }
+
+  async play(): Promise<void> {
+    const audioContext = new AudioContext();
+    const audioBuffer = await this.getSoundFile(audioContext, this.soundPath);
+    this.playSound(audioContext, audioBuffer);
+  }
 }
