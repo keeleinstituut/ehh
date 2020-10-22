916fdab54cbe0bea5ec0b5a3083085a21187d921
madistwn <madis.apsalon@twn.ee>
2020-10-22 14:30:41 +0300

    EHH-8 Modify checkup button component

--- src/app/components/checkup-button/checkup-button.component.ts
+++ src/app/components/checkup-button/checkup-button.component.ts
@@ -1,19 +1,21 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'ehh-checkup-button',
   templateUrl: './checkup-button.component.html',
   styleUrls: ['./checkup-button.component.scss']
 })
-export class CheckupButtonComponent implements OnInit {
+export class CheckupButtonComponent implements OnInit, OnChanges {
   @Output() check: EventEmitter<any> = new EventEmitter<any>();
   @Input() correct: boolean;
-  successClass = 'checkup--success';
-  failClass = 'checkup--fail';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
   }
 
 }
