5a9cdb57c2bf3617f5cf9cbc93acc3d0489d9074
Andre Luts <andre.luts@twn.ee>
2020-10-30 10:21:09 +0200

    ehh-20 resolved

--- src/app/containers/topic/components/topic-two/topic-two.component.ts
+++ src/app/containers/topic/components/topic-two/topic-two.component.ts
@@ -15,57 +15,57 @@ import {
 
 export class TopicTwoComponent implements OnInit {
   audioItems: AudioItem[] = [{
-      title: "n",
+      title: 'n',
       border: true,
-      audioURL: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3"
+      audioURL: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3'
     },
     {
-      title: "s",
+      title: 's',
       border: true,
-      audioURL: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3"
+      audioURL: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3'
     },
     {
-      title: "t",
+      title: 't',
       border: true,
-      audioURL: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3"
+      audioURL: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3'
     },
     {
-      title: "t/d",
+      title: 't/d',
       border: true,
-      audioURL: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3"
+      audioURL: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3'
     },
   ];
 
   audioItems2: AudioItem[] = [{
-      title: "n'",
+      title: 'n',
       border: true,
-      audioURL: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3"
+      audioURL: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3'
     },
     {
-      title: "s'",
+      title: 's',
       border: true,
-      audioURL: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3"
+      audioURL: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3'
     },
     {
-      title: "t'",
+      title: 't',
       border: true,
-      audioURL: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3"
+      audioURL: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3'
     },
     {
-      title: "t/d'",
+      title: 't/d',
       border: true,
-      audioURL: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3"
+      audioURL: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3'
     },
   ];
 
   buttonTitle = `
-  <span class="text-underline italic">
-  Võ<span class="bold">tt</span>is ka<span class="bold">ll</span>i kassi ja pa<span class="bold">n</span>i pa<span
-    class="bold">d</span>jale
+  <span class='text-underline italic'>
+  Võ<span class='bold'>tt</span>is ka<span class='bold'>ll</span>i kassi ja pa<span class='bold'>n</span>i pa<span
+    class='bold'>d</span>jale
   </span>
-  `
+  `;
 
-  buttonTitle2 = `<span class="text-underline">esineb</span>`
+  buttonTitle2 = `<span class='text-underline'>esineb</span>`;
 
   constructor() {}
 
