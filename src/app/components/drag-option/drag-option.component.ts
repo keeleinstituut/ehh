f9142aa82f6d3aec43ee7e372fe54679f2fb2b14
madistwn <madis.apsalon@twn.ee>
2020-11-18 16:25:29 +0200

    EHH-39 Add new option drag styles

---
+++ src/app/components/drag-option/drag-option.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ehh-drag-option',
+  templateUrl: './drag-option.component.html',
+  styleUrls: ['./drag-option.component.scss']
+})
+export class DragOptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
