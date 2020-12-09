2f269a00908e2c5e58e7a196e7c182421052c110
madistwn <madis.apsalon@twn.ee>
2020-12-09 13:25:56 +0200

    EHH-74 Modify image displaying

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   domainHost: 'https://elo.eki.ee',
   baseUrl: 'etLex/api/v1.0',
-  audioMainUrl: 'http://heliraamat.eki.ee/harjutused/Helid/seletused/',
-  imageMainUrl: 'http://heliraamat.eki.ee/harjutused/Pildid/',
+  audioMainUrl: 'http://heliraamat.eki.ee/harjutused/Helid/seletused',
+  imageMainUrl: 'http://heliraamat.eki.ee/harjutused/Pildid',
 };
 
 /*
