6fb8074e077e8e0d0aa9bd969944560811fbdd78
rabauti <rabauti@gmail.com>
2021-11-08 22:19:52 +0200

    EHH005 Fixed typo

--- src/app/containers/topic/components/topic-three/topic-three.component.spec.ts
+++ src/app/containers/topic/components/topic-three/topic-three.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TopicThreeComponent } from './topic-three.component';
 
-describe('TopicTwoComponent', () => {
+describe('TopicThreeComponent', () => {
   let component: TopicThreeComponent;
   let fixture: ComponentFixture<TopicThreeComponent>;
 
